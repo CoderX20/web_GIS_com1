48c6a9d0402e8549e0018726520d028a1f569562
Json <gx57220@163.com>
2022-08-10 15:14:46 +0800

    tempCode

--- QueryPointsRouter.js
+++ QueryPointsRouter.js
@@ -1,5 +1,7 @@
 const express=require("express")
 const mysql=require("mysql")
+const fs=require("fs")
+const path=require("path")
 const time=require("./node_time_set1")
 const Query=express.Router()
 var sql_config={}
@@ -7,6 +9,12 @@ var sql_config={}
 Query.post("/queryPoint",function(request,response){
     var id=request.query.id
     console.log("Query point id="+id+" when "+time.get_time_now())
+    fs.readFile(path.join(__dirname,"./DataInfor/onePoints1.json"),"utf-8",function(err,data){
+        if(err){
+            throw err
+        }
+        response.send(JSON.parse(data))
+    })
 })
 
 module.exports=Query
